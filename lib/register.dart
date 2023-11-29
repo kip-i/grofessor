5ca4e60dab0f0352441a5cfefb15e90bc16b7d2d
ryu-k67 <o2ro8chi@gmail.com>
2023-11-29 19:21:04 +0900

    add: アバター選択機能

--- lib/register.dart
+++ lib/register.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter_cube/flutter_cube.dart';
+import 'package:flutter_cube/flutter_cube.dart';
 
 
 class RegisterPage extends StatefulWidget {
@@ -10,7 +10,8 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   String _userName = '';
-  final String _password = 'password';
+  bool _isSelected1 = false;
+  bool _isSelected2 = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,59 +21,214 @@ class _RegisterPageState extends State<RegisterPage> {
         title: Text('Grofessor'),
         backgroundColor: Colors.green,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-                decoration: const InputDecoration(labelText: 'ユーザーネーム'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:[
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Center(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              child:TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'ユーザーネーム',
+                  labelStyle: TextStyle(color: Colors.green),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                    ),
+                  ),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+                style: TextStyle(
+                  color: Colors.green,
+                ),
                 onChanged: (String value) {
                   setState(() {
                     _userName = value;
                   });
                 },
               ),
-            SizedBox(height: 16.0),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //   children: [
-            //     Cube(
-            //       onSceneCreated: (Scene scene) {
-            //         scene.world.add(Object(fileName: "lib/assets/sampleObject.obj"));
-            //       },
-            //     ),
-            //     Cube(
-            //       onSceneCreated: (Scene scene) {
-            //         scene.world.add(Object(fileName: "lib/assets/sampleObject.obj"));
-            //       },
-            //     ),
-            //   ],
-            // ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  String _email = _userName+'@example.com';
-                  final User? user = (await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                              email: _email,
-                              password: _password
-                            )).user;
-                  if (user != null)
-                    print("ユーザ登録しました ${user.email} , ${user.uid}");
-                } catch (e) {
-                  print("ユーザ登録に失敗しました ${e}");
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-                foregroundColor: Colors.white
-              ),
-              child: const Text('作成'),
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 50.0),
+          Text(
+            'アバターを選択してください',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              // fontSize: 20,
+              color: Colors.green,
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // mainAxisSize: MainAxisSize.max,
+            children:[
+              Column(
+                children:[
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _isSelected1 = true;
+                        _isSelected2 = false;
+                      });
+                      print('アバター1が選択されました');
+                    },
+                    child:Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: _isSelected1 ? Colors.green : Colors.white, // 背景色を灰色に設定
+                      ),
+                      child: Cube(
+                        onSceneCreated: (Scene scene) {
+                          scene.world.add(Object(
+                            fileName: 'assets/models/v4.obj',
+                            scale: Vector3(5.0, 5.0, 5.0),
+                            rotation: Vector3(270.0, 180.0, 0.0),
+                          ));
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                    'アバター1',
+                    style: TextStyle(
+                      color: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
+              // SizedBox(width: 16),
+              Column(
+                children:[
+                  GestureDetector(
+                    onTap: () {
+                      // クリック時の処理
+                      setState(() {
+                        _isSelected1 = false;
+                        _isSelected2 = true;
+                      });
+                      print('アバター2が選択されました');
+                    },
+                    child:Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: _isSelected2 ? Colors.green : Colors.white, // 背景色を灰色に設定
+                      ),
+                      child: Cube(
+                        onSceneCreated: (Scene scene) {
+                          scene.world.add(Object(
+                            fileName: 'assets/models/v4.obj',
+                            scale: Vector3(5.0, 5.0, 5.0),
+                            rotation: Vector3(270.0, 180.0, 0.0),
+                          ));
+                        },
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'アバター2',
+                    style: TextStyle(
+                      color: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
+            ]
+          ),
+          SizedBox(height: 40.0),
+          ElevatedButton(
+            onPressed: _userName=='' || (!_isSelected1 && !_isSelected2) ? null : () async {
+              try {
+                String _email = _userName+'@example.com';
+                String _password = 'password';
+                final User? user = (await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                            email: _email,
+                            password: _password
+                          )).user;
+                if (user != null)
+                  print("ユーザ登録しました ${user.email} , ${user.uid}");
+              } catch (e) {
+                print("ユーザ登録に失敗しました ${e}");
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green,
+              foregroundColor: Colors.white
+            ),
+            child: const Text('作成'),
+          ),
+          SizedBox(height: 20.0),
+        ] 
       ),
+      //       SizedBox(height: 16.0),
+      //       // Row(
+      //       //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //         // children: [
+      //           // Text('アバター1'),
+              
+      //           // SizedBox(
+      //           //   width: 100,
+      //           //   height: 100,
+      //           //   child: Cube(
+      //           //     onSceneCreated: (Scene scene) {
+      //           //       scene.world.add(Object(
+      //           //         fileName: 'assets/models/v4.obj',
+      //           //       ));
+      //           //     },
+      //           //   ),
+      //           // ),
+      //           // SizedBox(
+      //           //   width: 100,
+      //           //   height: 100,
+      //           //   child: Cube(
+      //           //     onSceneCreated: (Scene scene) {
+      //           //       scene.world.add(Object(
+      //           //         fileName: 'assets/models/cube.obj',
+      //           //       ));
+      //           //     },
+      //           //   ),
+      //           // ),
+      //           // Text('アバター2'),
+      //         // ],
+      //       // ),
+      //       SizedBox(height: 16.0),
+      //       ElevatedButton(
+      //         onPressed: () async {
+      //           try {
+      //             String _email = _userName+'@example.com';
+      //             String _password = 'password';
+      //             final User? user = (await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      //                         email: _email,
+      //                         password: _password
+      //                       )).user;
+      //             if (user != null)
+      //               print("ユーザ登録しました ${user.email} , ${user.uid}");
+      //           } catch (e) {
+      //             print("ユーザ登録に失敗しました ${e}");
+      //           }
+      //         },
+      //         style: ElevatedButton.styleFrom(
+      //           backgroundColor: Colors.green,
+      //           foregroundColor: Colors.white
+      //         ),
+      //         child: const Text('作成'),
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
