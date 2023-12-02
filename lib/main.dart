38ef9f14fb591ad57b6883aae3b0f1c9ce159ad2
ryu-k67 <o2ro8chi@gmail.com>
2023-12-02 15:54:47 +0900

    fix: mainの修正

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-import 'register.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
