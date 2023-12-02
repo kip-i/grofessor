273f0e0a13c9173344fc33166513fc65782cea3c
zgmfx56s <220445010@ccmailg.meijo-u.ac.jp>
2023-12-02 17:42:49 +0900

    add:checkのサイズを大きくした。

--- lib/schedule.dart
+++ lib/schedule.dart
@@ -15,12 +15,11 @@ class YouApp extends StatelessWidget {
           title: Text('時間割表',style:TextStyle(
                   color:Color.fromARGB(255, 10, 98, 11),
                   fontStyle:FontStyle.italic,
+                  fontSize:30.0,
                   )),
                  
         ),
-        body:
-        
-        
+        body:        
          SingleChildScrollView(
           scrollDirection:Axis.vertical,
           child:FittedBox(child: MyDataTable(),)),
@@ -43,119 +42,130 @@ class _MyDataTableState extends State<MyDataTable> {   //_MyDataTableStateクラ
   @override
   Widget build(BuildContext context) {
     return DataTable(
-      dataRowMaxHeight:150.0,
-      decoration: BoxDecoration(color: Color.fromARGB(255, 170, 194, 170),),
+      dataRowMaxHeight:170.0,
+      decoration: BoxDecoration(color: Color.fromARGB(255, 195, 199, 195),),
       columns: [
         DataColumn(label: Text('')),
-        DataColumn(label: Text('  月',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:24),)),
-        DataColumn(label: Text('  火',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:24),)),
-        DataColumn(label: Text('  水',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:24),)),
-        DataColumn(label: Text('  木',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:24),)),
-        DataColumn(label: Text('  金',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:24),)),
-        DataColumn(label: Text('  土',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:24),)),
+        DataColumn(label: Text('  月',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:27),)),
+        DataColumn(label: Text('  火',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:27),)),
+        DataColumn(label: Text('  水',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:27),)),
+        DataColumn(label: Text('  木',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:27),)),
+        DataColumn(label: Text('  金',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:27),)),
+        DataColumn(label: Text('  土',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:27),)),
       ],
       rows: [
         DataRow(color: MaterialStateProperty.resolveWith((states) {
           return Colors.white;}),        //一限目
           cells: [
-            DataCell(Text('1',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:24),)),
-            DataCell(Checkbox(value: _isSelected[0],onChanged: (bool? value) {
-                  setState(() {_isSelected[0] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[6],onChanged: (bool? value) {
-                  setState(() {_isSelected[6] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[12],onChanged: (bool? value) {
-                  setState(() {_isSelected[12] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[18],onChanged: (bool? value) {
-                  setState(() {_isSelected[18] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[24],onChanged: (bool? value) {
-                  setState(() {_isSelected[24] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[30],onChanged: (bool? value) {
-                  setState(() {_isSelected[30] = value!;});}),),
+            
+            DataCell(Text('1',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:28),)),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[0],onChanged: (bool? value) {
+                  setState(() {_isSelected[0] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[6],onChanged: (bool? value) {
+                  setState(() {_isSelected[6] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[12],onChanged: (bool? value) {
+                  setState(() {_isSelected[12] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[18],onChanged: (bool? value) {
+                  setState(() {_isSelected[18] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[24],onChanged: (bool? value) {
+                  setState(() {_isSelected[24] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[30],onChanged: (bool? value) {
+                  setState(() {_isSelected[30] = value!;});}),),),
                ],),
+        
         DataRow(color: MaterialStateProperty.resolveWith((states) {
           return Colors.white;}),        //二限目
           cells: [
-            DataCell(Text('2',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:24),)),
-            DataCell(Checkbox(value: _isSelected[1],onChanged: (bool? value) {
-                  setState(() {_isSelected[1] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[7],onChanged: (bool? value) {
-                  setState(() {_isSelected[7] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[13],onChanged: (bool? value) {
-                  setState(() {_isSelected[13] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[19],onChanged: (bool? value) {
-                  setState(() {_isSelected[19] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[25],onChanged: (bool? value) {
-                  setState(() {_isSelected[25] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[31],onChanged: (bool? value) {
-                  setState(() {_isSelected[31] = value!;});}),),
+            
+            DataCell(Text('2',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:28),)),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[1],onChanged: (bool? value) {
+                  setState(() {_isSelected[1] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[7],onChanged: (bool? value) {
+                  setState(() {_isSelected[7] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[13],onChanged: (bool? value) {
+                  setState(() {_isSelected[13] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[19],onChanged: (bool? value) {
+                  setState(() {_isSelected[19] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[25],onChanged: (bool? value) {
+                  setState(() {_isSelected[25] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[31],onChanged: (bool? value) {
+                  setState(() {_isSelected[31] = value!;});}),),),
                   ],),
+        
         DataRow(color: MaterialStateProperty.resolveWith((states) {
           return Colors.white;}),       
           cells: [
-            DataCell(Text('3',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:24),)),
-            DataCell(Checkbox(value: _isSelected[2],onChanged: (bool? value) {
-                  setState(() {_isSelected[2] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[8],onChanged: (bool? value) {
-                  setState(() {_isSelected[8] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[14],onChanged: (bool? value) {
-                  setState(() {_isSelected[14] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[20],onChanged: (bool? value) {
-                  setState(() {_isSelected[20] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[26],onChanged: (bool? value) {
-                  setState(() {_isSelected[26] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[32],onChanged: (bool? value) {
-                  setState(() {_isSelected[32] = value!;});}),),
+            
+            DataCell(Text('3',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:28),)),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[2],onChanged: (bool? value) {
+                  setState(() {_isSelected[2] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[8],onChanged: (bool? value) {
+                  setState(() {_isSelected[8] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[14],onChanged: (bool? value) {
+                  setState(() {_isSelected[14] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[20],onChanged: (bool? value) {
+                  setState(() {_isSelected[20] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[26],onChanged: (bool? value) {
+                  setState(() {_isSelected[26] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[32],onChanged: (bool? value) {
+                  setState(() {_isSelected[32] = value!;});}),),),
                   ],),
-DataRow(color: MaterialStateProperty.resolveWith((states) {
-          return Colors.white;}),       
-          cells: [
-            DataCell(Text('4',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),),)),
-            DataCell(Checkbox(value: _isSelected[3],onChanged: (bool? value) {
-                  setState(() {_isSelected[3] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[9],onChanged: (bool? value) {
-                  setState(() {_isSelected[9] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[15],onChanged: (bool? value) {
-                  setState(() {_isSelected[15] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[21],onChanged: (bool? value) {
-                  setState(() {_isSelected[21] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[27],onChanged: (bool? value) {
-                  setState(() {_isSelected[27] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[33],onChanged: (bool? value) {
-                  setState(() {_isSelected[33] = value!;});}),),
-                  ],),
+
         DataRow(color: MaterialStateProperty.resolveWith((states) {
           return Colors.white;}),       
           cells: [
-            DataCell(Text('5',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),),)),
-            DataCell(Checkbox(value: _isSelected[4],onChanged: (bool? value) {
-                  setState(() {_isSelected[4] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[10],onChanged: (bool? value) {
-                  setState(() {_isSelected[10] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[16],onChanged: (bool? value) {
-                  setState(() {_isSelected[16] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[22],onChanged: (bool? value) {
-                  setState(() {_isSelected[22] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[28],onChanged: (bool? value) {
-                  setState(() {_isSelected[28] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[34],onChanged: (bool? value) {
-                  setState(() {_isSelected[34] = value!;});}),),
+            
+            DataCell(Text('4',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:28),)),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[3],onChanged: (bool? value) {
+                  setState(() {_isSelected[3] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[9],onChanged: (bool? value) {
+                  setState(() {_isSelected[9] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[15],onChanged: (bool? value) {
+                  setState(() {_isSelected[15] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[21],onChanged: (bool? value) {
+                  setState(() {_isSelected[21] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[27],onChanged: (bool? value) {
+                  setState(() {_isSelected[27] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[33],onChanged: (bool? value) {
+                  setState(() {_isSelected[33] = value!;});}),),),
                   ],),
+        
         DataRow(color: MaterialStateProperty.resolveWith((states) {
           return Colors.white;}),       
           cells: [
-            DataCell(Text('6',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),),)),
-            DataCell(Checkbox(value: _isSelected[5],onChanged: (bool? value) {
-                  setState(() {_isSelected[5] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[11],onChanged: (bool? value) {
-                  setState(() {_isSelected[11] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[17],onChanged: (bool? value) {
-                  setState(() {_isSelected[17] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[23],onChanged: (bool? value) {
-                  setState(() {_isSelected[23] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[29],onChanged: (bool? value) {
-                  setState(() {_isSelected[29] = value!;});}),),
-            DataCell(Checkbox(value: _isSelected[35],onChanged: (bool? value) {
-                  setState(() {_isSelected[35] = value!;});}),),
+            
+            DataCell(Text('5',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:28),)),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[4],onChanged: (bool? value) {
+                  setState(() {_isSelected[4] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[10],onChanged: (bool? value) {
+                  setState(() {_isSelected[10] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[16],onChanged: (bool? value) {
+                  setState(() {_isSelected[16] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[22],onChanged: (bool? value) {
+                  setState(() {_isSelected[22] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[28],onChanged: (bool? value) {
+                  setState(() {_isSelected[28] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[34],onChanged: (bool? value) {
+                  setState(() {_isSelected[34] = value!;});}),),),
+                  ],),
+        
+        DataRow(color: MaterialStateProperty.resolveWith((states) {
+          return Colors.white;}),       
+          cells: [
+            
+            DataCell(Text('6',style:TextStyle(color:Color.fromARGB(255, 10, 98, 11),fontSize:28),)),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[5],onChanged: (bool? value) {
+                  setState(() {_isSelected[5] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[11],onChanged: (bool? value) {
+                  setState(() {_isSelected[11] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[17],onChanged: (bool? value) {
+                  setState(() {_isSelected[17] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[23],onChanged: (bool? value) {
+                  setState(() {_isSelected[23] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[29],onChanged: (bool? value) {
+                  setState(() {_isSelected[29] = value!;});}),),),
+            DataCell(Transform.scale(scale:2.0,child:Checkbox(value: _isSelected[35],onChanged: (bool? value) {
+                  setState(() {_isSelected[35] = value!;});}),),),
                   ],),
       ],
     );
