19495c97d57e03cd87a95bcafbef1ce1361239a4
kip-i <kppisj@gmail.com>
2023-12-07 03:20:18 +0900

    update: 計測開始ボタンの改善

--- lib/home/measurements_start.dart
+++ lib/home/measurements_start.dart
@@ -6,15 +6,17 @@ class MeasurementsStart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 140.0, // 幅を指定
-      height: 60.0, // 高さを指定
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: blackbordFrameColor,
-          width: 4.0,
-        ),
-        borderRadius: BorderRadius.circular(8.0), // 角を丸くする
-      ),
+      width: 160.0, // 幅を指定
+      height: 70.0, // 高さを指定
+      // decoration: BoxDecoration(
+      //   border: Border.all(
+      //     color: blackbordFrameColor,
+      //     width: 4.0,
+      //   ),
+      //   borderRadius: BorderRadius.circular(8.0), // 角を丸くする
+      // ),
+      // decoration: BoxDecoration(
+      //   border: Border(bottom: BorderSide(color: blackbordFrameColor, width: 4.0)),),
       child: ElevatedButton(
         onPressed: () {
           // ボタンが押された時の処理を追加
@@ -22,16 +24,33 @@ class MeasurementsStart extends StatelessWidget {
         style: ElevatedButton.styleFrom(
           primary: blackbordColor, // ボタンの背景色
           onPrimary: blackbordWhiteColor, // テキストの色
-          padding: EdgeInsets.all(4.0), // ボタンの内側の余白
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(4.0), // ボタンの角の丸み
+          padding: EdgeInsets.all(6.0), // ボタンの内側の余白
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Icon(
+            Icons.timer,
+            size: 45.0,
+            color:  blackbordWhiteColor,
           ),
-        ),
-        child: Text(
-          '計測開始',
-          style: TextStyle(fontSize: 24.0),
-        ),
+          Container(
+            margin: EdgeInsets.all(4.0), 
+            child: Text(
+              '計測開始',
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+        ]
+        ,)
+        
+      
       ),
     );
   }
 }
+
+//使えそうなIcon
+//Play Arrow, timer
