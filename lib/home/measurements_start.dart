8cf57b21bde64ce39e37738c37c4cca71b2305bf
kip-i <kppisj@gmail.com>
2023-12-07 02:27:27 +0900

    feat: 計測開始ボタンの黒板化

--- lib/home/measurements_start.dart
+++ lib/home/measurements_start.dart
@@ -8,21 +8,28 @@ class MeasurementsStart extends StatelessWidget {
     return Container(
       width: 140.0, // 幅を指定
       height: 60.0, // 高さを指定
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: blackbordFrameColor,
+          width: 4.0,
+        ),
+        borderRadius: BorderRadius.circular(8.0), // 角を丸くする
+      ),
       child: ElevatedButton(
         onPressed: () {
           // ボタンが押された時の処理を追加
         },
         style: ElevatedButton.styleFrom(
-          primary: accentColor, // ボタンの背景色
-          onPrimary: Colors.white, // テキストの色
-          padding: EdgeInsets.all(16.0), // ボタンの内側の余白
+          primary: blackbordColor, // ボタンの背景色
+          onPrimary: blackbordWhiteColor, // テキストの色
+          padding: EdgeInsets.all(4.0), // ボタンの内側の余白
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0), // ボタンの角の丸み
+            borderRadius: BorderRadius.circular(4.0), // ボタンの角の丸み
           ),
         ),
         child: Text(
           '計測開始',
-          style: TextStyle(fontSize: 20.0),
+          style: TextStyle(fontSize: 24.0),
         ),
       ),
     );
