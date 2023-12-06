5b85e8806843799e7f0a8dafef788bb7301a0ab4
kip-i <kppisj@gmail.com>
2023-12-06 19:28:49 +0900

    update: 選択された時の背景色の固定化

--- lib/const/color.dart
+++ lib/const/color.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 const accentColor = Color(0xFF00753F);
+const selectedColor = Color.fromARGB(255, 0, 140, 75);
 const unselectedColor = Color.fromARGB(255, 80, 81, 80);
