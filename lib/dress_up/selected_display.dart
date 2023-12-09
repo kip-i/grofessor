b8fd87c75574a835e738d880675c14dae3813e89
kip-i <kppisj@gmail.com>
2023-12-09 16:54:37 +0900

    [wip] feat: 一時保存

--- lib/dress_up/selected_display.dart
+++ lib/dress_up/selected_display.dart
@@ -16,8 +16,9 @@ class _SelectedDisplayState extends State<SelectedDisplay> {
   Widget build(BuildContext context) {
     final dataProvider = Provider.of<DataProvider>(context);
     dataProvider.getCharacterId();
+    dataProvider.getBackgroundId();
     return Stack(
-      children: [
+      children:[
         Positioned(
           child: Container(
             width: MediaQuery.of(context).size.width,
@@ -29,19 +30,43 @@ class _SelectedDisplayState extends State<SelectedDisplay> {
             )),
         ),),
         Positioned(
-          child: Container(
-            child: Cube(
-              onSceneCreated: (Scene scene) {
-                scene.world.add(Object(
-                  fileName: 'assets/models/'+dataProvider.characterId.toString()+'.obj',
-                  scale: Vector3(13.0, 13.0, 13.0),
-                  rotation: Vector3(270.0, 180.0, 0.0),
-                  position: Vector3(-0.9, -4.0, 0.0),
-                ));
-              },
-            ),
-          )
-        )
+          child: Cube(
+            onSceneCreated: (Scene scene) {
+              scene.world.add(Object(
+                fileName: 'assets/models/'+dataProvider.characterId.toString()+'.obj',
+                scale: Vector3(13.0, 13.0, 13.0),
+                rotation: Vector3(270.0, 180.0, 0.0),
+                position: Vector3(-0.9, -4.0, 0.0),
+              ));
+            },
+          ),
+          // Container(
+          //   // TODO: モデルが書き変わらない
+          //   child: Consumer<DataProvider>(
+          //     builder: (context, dataProvider, child) {
+          //       return Cube(
+          //         onSceneCreated: (Scene scene) {
+          //           scene.world.add(Object(
+          //             fileName: 'assets/models/'+dataProvider.characterId.toString()+'.obj',
+          //             scale: Vector3(13.0, 13.0, 13.0),
+          //             rotation: Vector3(270.0, 180.0, 0.0),
+          //             position: Vector3(-0.9, -4.0, 0.0),
+          //           ));
+          //         },
+          //       );
+          //     },
+          //   ),
+          // )
+        ),
+        Positioned(
+        child: Text(
+          '検知はできてる' + dataProvider.characterId.toString() + '.png',
+          style: TextStyle(
+            fontSize: 50.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
       ]
     );
   }
