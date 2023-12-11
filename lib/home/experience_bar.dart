cb4ebd248d2ca4a538e55dc4ac191cf8138cc6ef
kip-i <kppisj@gmail.com>
2023-12-12 00:09:25 +0900

    update: 各種providerに対応

--- lib/home/experience_bar.dart
+++ lib/home/experience_bar.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:grofessor/_state.dart';
+import 'package:grofessor/state.dart';
+import 'package:provider/provider.dart';
 
 class ExperienceBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final achieveProvider = Provider.of<AchieveProvider>(context);
     return Container(
       width: 180.0,
       height: 13.0,
@@ -13,7 +17,7 @@ class ExperienceBar extends StatelessWidget {
           borderRadius: BorderRadius.circular(8.0), // カードの角を丸める
         ),
         child: LinearProgressIndicator(
-          value: 0.5,
+          value: achieveProvider.thisTime / achieveProvider.needTime,
           backgroundColor: Colors.grey[300], // プログレスバーの背景色
           valueColor: AlwaysStoppedAnimation<Color>(Colors.blue), // プログレスバーの色
         ),
