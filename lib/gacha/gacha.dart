8bdb5de9b0b8652b1bcf87d12da9e7ce6bbfbd47
kip-i <kppisj@gmail.com>
2023-12-12 09:45:45 +0900

    feat: ある程度のスタックに対応

--- lib/gacha/gacha.dart
+++ lib/gacha/gacha.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'dart:math';
-import '../state.dart';
+import '../_state.dart';
 import 'showdialog_background.dart';
 import 'showdialog_nickname.dart';
 import 'showdialog_skin.dart';
@@ -68,247 +68,273 @@ class GachaPageState extends State<GachaPage> {
 
   @override
   Widget build(BuildContext context) {
-    final dataProvider = Provider.of<DataProvider>(context);
-    int x = dataProvider.paperNum;
-    int y = dataProvider.gachaTicket;
+    final gachaProvider = Provider.of<GachaProvider>(context);
+    final achieveProvider = Provider.of<AchieveProvider>(context);
+    final userProvider = Provider.of<UserProvider>(context);
+    int x = achieveProvider.paperNum;
+    int y = gachaProvider.gachaTicket;
+    // Debug用
+    y = 100;
+    gachaProvider.setGachaTicket(userProvider.userId, 1);
     void generateRandomString() {
-      print('なにもないよ');
-      List<String> elements = dataProvider.notHaveNickNameIdList +
-          dataProvider.notHaveCharacterIdList +
-          dataProvider.notHaveBackgroundIdList;
+      print('リスト生成'+gachaProvider.gachaTicket.toString());
+      List<String> elements = 
+          //gachaProvider.notHaveNickNameIdList +
+          //gachaProvider.notHaveNickNameIdList +
+          gachaProvider.notHaveCharacterIdList ;
+          //gachaProvider.notHaveBackgroundIdList +
+          //gachaProvider.notHaveBackgroundIdList;
+      print('y'+y.toString());
+      print(gachaProvider.notHaveBackgroundIdList);
+      print(gachaProvider.notHaveNickNameIdList);
+      print(gachaProvider.notHaveCharacterIdList);
+
       if (elements.isEmpty || y <= 0) {
         setState(() {
           randomString = 'もう出ないよ'; // 全ての要素が表示されたらガチャが引けなくなる
         });
         return;
       }
+      print('あああ');
+      
 
       Random random = Random();
       int randomIndex = random.nextInt(elements.length);
       String selectedElement = elements[randomIndex];
-
+      print('引く前'+ gachaProvider.gachaTicket.toString());
       setState(() {
         randomString = selectedElement;
         y = y - 1; // ガチャを引いたら'y'の値を1減らす
+        gachaProvider.setGachaTicket(userProvider.userId, -1);
       });
-      print(selectedElement);
+      print('引く後' + gachaProvider.gachaTicket.toString());
+      print('ガチャ排出：'+selectedElement.toString());
       print(elements);
       if (selectedElement.startsWith('b')) {
-        dataProvider.setNotHaveBackgroundIdList(selectedElement);
-        showDialog_background(context);
+        gachaProvider.setNotHaveBackgroundIdList(userProvider.userId, selectedElement);
+        showDialog_background(context, selectedElement);
       } else if (selectedElement.startsWith('n')) {
-        dataProvider.setNotHaveNickNameIdList(selectedElement);
-        showDialog_nickname(context);
+        gachaProvider.setNotHaveNickNameIdList(
+            userProvider.userId, selectedElement);
+        showDialog_nickname(context, selectedElement);
       }else{
-        dataProvider.setNotHaveCharacterIdList(selectedElement);
-        showDialog_Skin(context);
+        gachaProvider.setNotHaveCharacterIdList(
+            userProvider.userId, selectedElement);
+        showDialog_Skin(context, selectedElement);
       }
     }
 
-    return Scaffold(
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              children: [
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Image.asset(
-                       'assets/images/kirakira.jpg',
-                      width: 380,
-                      height: 380,
-                      fit: BoxFit.cover,
-                    ),
-                   // SizedBox(height: 50),//プレゼントの高さを変える
-                   Positioned(
-                    top: 120, // Adjust this value to change the vertical position
-                    child: Image.asset(
-                      'assets/images/present.jpg',
-                      width: 195,
-                      height: 195,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ],
+    return Stack(
+      children: [
+        Positioned(
+          top: 0,
+          left: 0,
+          child: 
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/kirakira.png'),
+                  fit: BoxFit.cover,
+                ),
               ),
-              SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: generateRandomString,
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(0.0),
-                    ),
-                    primary: Colors.transparent,
-                    onPrimary: Colors.transparent,
-                    side: BorderSide(color: Colors.transparent, width: 5),
-                  ),
-                  child: Container(
-                    width: 200,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 238, 238, 238),
-                      border: Border(
-                        top: BorderSide(color: Colors.red),
-                        left: BorderSide(color: Colors.red, width: 15),
-                        right: BorderSide(color: Colors.green, width: 15),
-                        bottom: BorderSide(color: Colors.green),
-                      ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'ガチャを引く',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
+            )
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.width/2.3, // Adjust this value to change the vertical position
+          left: 100,
+          child: Image.asset(
+            'assets/images/present.png',
+            width: MediaQuery.of(context).size.width-200,
+            height: MediaQuery.of(context).size.width-200,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.height/2, // Adjust this value to change the vertical position
+          left: MediaQuery.of(context).size.width/2-110,
+          child: ElevatedButton(
+            onPressed: generateRandomString,
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0.0),
+              ),
+              primary: Colors.transparent,
+              onPrimary: Colors.transparent,
+              side: BorderSide(color: Colors.transparent, width: 5),
+            ),
+            child: Container(
+              width: 200,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 238, 238, 238),
+                border: Border(
+                  top: BorderSide(color: Colors.red),
+                  left: BorderSide(color: Colors.red, width: 15),
+                  right: BorderSide(color: Colors.green, width: 15),
+                  bottom: BorderSide(color: Colors.green),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  'ガチャを引く',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(height: 40),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top:MediaQuery.of(context).size.height/2 + 80,
+          left: 30,
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '論文の数  :  $x',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.height / 2 + 80,
+          right: 30 ,
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '引けるの数  :  $y ',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.height / 2 + 150,
+          left: 50,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            controller: _scrollController1,
+            child: Row(
+              children: [
                 Container(
+                  width: 150,
+                  height: 50,
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
+                    color: Colors.green,
                     borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
+                  child: Center(
                     child: Text(
-                      '現在の提出論文数  :  $x',
+                      '    提 出 論 文 数   ➡   ',
                       style: TextStyle(
+                        color: const Color.fromARGB(255, 255, 233, 233),
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
                 ),
-                SizedBox(height: 8),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      '引けるガチャの数  :  $y ',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
+                SizedBox(
+                  width: 15,
                 ),
-                SizedBox(height: 30),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  controller: _scrollController1,
-                  child: Row(
+                for (var i = 0; i < 300; i++)
+                  Stack(
                     children: [
-                      Container(
-                        width: 150,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                          borderRadius: BorderRadius.circular(10),
+                      Column(
+                        children: [
+                          Container(
+                            color: (i + 1) % 3 == 0
+                                ? Colors.yellow
+                                : i.isEven
+                                    ? Color.fromARGB(255, 116, 203, 44)
+                                    : Color.fromARGB(255, 116, 203, 44),
+                            width: 110,
+                            height: 110,
+                            child: Center(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    (i + 1).toString(),
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 30,
+                                    ),
+                                  ),
+                                  SizedBox(width: 5),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      if (i + 1 <= achieveProvider.paperNum)
+                        Positioned(
+                          top: 38,
+                          right: 40,
+                          child: Transform.scale(
+                            scale: 1.5,
+                            child: Icon(Icons.check, color: Colors.black, size: 24 * 1.2),
+                          ),
                         ),
-                        child: Center(
-                          child: Text(
-                            '    提 出 論 文 数   ➡   ',
-                            style: TextStyle(
-                              color: const Color.fromARGB(255, 255, 233, 233),
-                              fontWeight: FontWeight.bold,
+                      if ((i + 1) % 3 == 0)
+                        Positioned(
+                          top: 5,
+                          right: 5,
+                          child: Transform.scale(
+                            scale: 1.2,
+                            child: Icon(Icons.star, color: Color.fromARGB(255, 200, 210, 18)),
+                          ),
+                        ),
+                      if ((i + 1) % 3 == 0)
+                        Positioned(
+                          top: 75,
+                          right: -3,
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Colors.yellow,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Text(
+                              'ごほうびゲット',
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 0, 0, 0),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      for (var i = 0; i < 300; i++)
-                        Stack(
-                          children: [
-                            Column(
-                              children: [
-                                Container(
-                                  color: (i + 1) % 3 == 0
-                                      ? Colors.yellow
-                                      : i.isEven
-                                          ? Color.fromARGB(255, 116, 203, 44)
-                                          : Color.fromARGB(255, 116, 203, 44),
-                                  width: 110,
-                                  height: 110,
-                                  child: Center(
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          (i + 1).toString(),
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 30,
-                                          ),
-                                        ),
-                                        SizedBox(width: 5),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            if (i + 1 <= dataProvider.paperNum)
-                              Positioned(
-                                top: 38,
-                                right: 40,
-                                child: Transform.scale(
-                                  scale: 1.5,
-                                  child: Icon(Icons.check, color: Colors.black, size: 24 * 1.2),
-                                ),
-                              ),
-                            if ((i + 1) % 3 == 0)
-                              Positioned(
-                                top: 5,
-                                right: 5,
-                                child: Transform.scale(
-                                  scale: 1.2,
-                                  child: Icon(Icons.star, color: Color.fromARGB(255, 200, 210, 18)),
-                                ),
-                              ),
-                            if ((i + 1) % 3 == 0)
-                              Positioned(
-                                top: 75,
-                                right: -3,
-                                child: Container(
-                                  padding: EdgeInsets.all(8),
-                                  decoration: BoxDecoration(
-                                    color: Colors.yellow,
-                                    borderRadius: BorderRadius.circular(5),
-                                  ),
-                                  child: Text(
-                                    'ごほうびゲット',
-                                    style: TextStyle(
-                                      color: const Color.fromARGB(255, 0, 0, 0),
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                          ],
-                        ),
                     ],
                   ),
-                ),
               ],
             ),
-          ),
-        ],
-      ),
+        )
+        ),
+
+      ],
     );
   }
-}
-
-mixin dataProvider {
 }
